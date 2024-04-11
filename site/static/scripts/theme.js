b5a3fd5650884e9c19f7ea59802c859434a54d9f
Anirudh Sudhir <anirudh.sudhir1@gmail.com>
2024-04-11 11:26:03 +0530

    feat: switch to purple theme

--- site/static/scripts/theme.js
+++ site/static/scripts/theme.js
@@ -1,18 +1,18 @@
 function lightScheme() {
   // change the tags
-  document.documentElement.style.setProperty("--background", "#f9f1cb");
-  document.documentElement.style.setProperty("--primary-colour", "#ac792e");
-  document.documentElement.style.setProperty("--font-colour", "#282828");
-  document.documentElement.style.setProperty("--tag-background", "#eee2be");
-  document.getElementById("theme-toggle").src="/static/icons/light-toggle.svg";
+  document.documentElement.style.setProperty("--background", "#FFFFFF");
+  document.documentElement.style.setProperty("--primary-colour", "#701adb");
+  document.documentElement.style.setProperty("--font-colour", "#000000");
+  document.documentElement.style.setProperty("--tag-background", "#f1f1f1");
+  document.getElementById("theme-toggle").src = "/static/icons/theme-toggle-purple-dark.svg";
 }
 
 function darkScheme() {
-  document.documentElement.style.setProperty("--background", "#1e2021");
-  document.documentElement.style.setProperty("--primary-colour", "#f0bf4f");
-  document.documentElement.style.setProperty("--font-colour", "#f9f1cb");
-  document.documentElement.style.setProperty("--tag-background", "#3b3836");
-  document.getElementById("theme-toggle").src="/static/icons/dark-toggle.svg";
+  document.documentElement.style.setProperty("--background", "#121212");
+  document.documentElement.style.setProperty("--primary-colour", "#BB86FC");
+  document.documentElement.style.setProperty("--font-colour", "#ffffff");
+  document.documentElement.style.setProperty("--tag-background", "#1f1f1f");
+  document.getElementById("theme-toggle").src = "/static/icons/theme-toggle-purple.svg";
 }
 
 // store user toggle preference in local storage. Site must remember what theme was last used
