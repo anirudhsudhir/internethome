ec9caf47548bd80882b152eafef61e32e3d130b9
Anirudh Sudhir <anirudh.sudhir1@gmail.com>
2024-12-31 15:50:55 +0530

    feat: update bitcask post and default theme
    multi-log compaction and benchmarks
    use media queries to identify default colorscheme

--- site/static/scripts/theme.js
+++ site/static/scripts/theme.js
@@ -4,13 +4,11 @@ function switchTheme(e) {
         currentTheme = "dark"
         document.documentElement.setAttribute('data-theme', currentTheme);
         localStorage.setItem('theme', currentTheme);
-        // toggleSwitch.innerHTML = `dark_mode`
     }
     else {
         currentTheme = "light"
         document.documentElement.setAttribute('data-theme', currentTheme);
         localStorage.setItem('theme', currentTheme);
-        // toggleSwitch.innerHTML = `light_mode`
     }
 }
 
@@ -20,9 +18,3 @@ toggleSwitch.addEventListener('click', switchTheme);
 
 let currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : "light";
 document.documentElement.setAttribute('data-theme', currentTheme);
-
-// if (currentTheme == "light") {
-//     toggleSwitch.innerHTML = `light_mode`
-// } else {
-//     toggleSwitch.innerHTML = `dark_mode`
-// }
